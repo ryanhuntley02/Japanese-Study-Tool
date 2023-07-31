aa234e867dc2c5d5e5f5fccc1800fac2dfb88d30
Ryan Huntley <rsh8867@rit.edu>
2023-07-31 03:13:30 -0400

    Refactor shuffle to preserve current card

--- js/flashcards.js
+++ js/flashcards.js
@@ -50,83 +50,43 @@ function loadSet(set) {
     $('.carousel-item')[0].className += ' active';
 }
 
-function loadshuffle(set){
-    
-    carousel = $(".carousel-inner")[0];
-    let activeCpy = JSON.parse(JSON.stringify(activeSet));
-    for(let i = 0; i<Object.keys(activeSet).length; i++){
-        let rand = getRand(Object.keys(activeCpy).length);
-        let key = Object.keys(activeCpy)[rand];
-
-        card = document.createElement("div");
-        card.setAttribute("class", 'carousel-item');
-
-        flipCard = document.createElement("div");
-        flipCard.setAttribute("class", "flip-card");
-
-        flipCardInner = document.createElement("div");
-        flipCardInner.setAttribute("class", "flip-card-inner");
-
-        flipCardFront = document.createElement("div");
-        flipCardFront.setAttribute('class', 'flip-card-front');
-
-        keyText = document.createElement("p");
-        keyText.innerHTML = key;
-        valueText = document.createElement("p");
-        valueText.innerHTML = activeCpy[key];
-
-        flipCardBack = document.createElement("div");
-        flipCardBack.setAttribute('class','flip-card-back');
-
-        flipCardBack.appendChild(valueText);
-        flipCardFront.appendChild(keyText);
-        flipCardInner.appendChild(flipCardFront);
-        flipCardInner.appendChild(flipCardBack);
-        flipCard.appendChild(flipCardInner);
-        card.appendChild(flipCard);
-        carousel.appendChild(card);
-        delete activeCpy[key];
-    }
-    $('.carousel-item')[0].className += ' active';
-}
-
 function shuffle(){
     $('.shuffle').on('click', shuffleOn);
 }
 
-// function reOrder(isShuffled){
-//     if(isShuffled){
-//         let activeCpy = JSON.parse(JSON.stringify(activeSet));
-//         for(card in $('.flip-card-inner')){
-//             let rand = getRand(Object.keys(activeCpy).length);
-//             let key = Object.keys(activeCpy)[rand];
-//             $('.flip-card-front p', card).html(key);
-//             $('.flip-card-back p', card).html(activeCpy[key]);
-//             delete activeCpy[key];
-//         }
-//     }
-//     else{
-//         for(let i = 0; i<Object.keys(activeSet).length; i++){
-//             let card = $('.flip-card-inner')[i];
-//             let key = Object.keys(activeSet)[i];
-//             $('.flip-card-front p', card).html(key);
-//             $('.flip-card-back p', card).html(activeSet[key]);
-//         }
-//     }
-// }
+function reOrder(isShuffled){
+    if(isShuffled){
+        let activeCpy = JSON.parse(JSON.stringify(activeSet));
+        $('.flip-card').each((card)=>{
+            console.log(card);
+            let rand = getRand(Object.keys(activeCpy).length);
+            let key = Object.keys(activeCpy)[rand];
+            console.log(key);
+            $('.flip-card-front p', card).html(key);
+            $('.flip-card-back p', card).html(activeCpy[key]);
+            delete activeCpy[key];
+        })
+    }
+    else{
+        for(let i = 0; i<Object.keys(activeSet).length; i++){
+            let card = $('.flip-card-inner')[i];
+            let key = Object.keys(activeSet)[i];
+            $('.flip-card-front p', card).html(key);
+            $('.flip-card-back p', card).html(activeSet[key]);
+        }
+    }
+}
 
 function shuffleOn(){
-    $('.carousel-item').remove();
     $(this).off('click', shuffleOn);
     $(this).on('click', shuffleOff);
-    loadshuffle(activeSet);
+    reOrder(true);
 }
 
 function shuffleOff(){
-    $('.carousel-item').remove();
     $(this).off('click', shuffleOff)
     $(this).on('click', shuffleOn);
-    loadSet(activeSet);
+    reOrder(false);
 }
 
 function getRand(max){
